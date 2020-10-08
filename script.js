7776e6b04fbd81dc0ccbabf170658c42b4a0c795
arzhk <72197750+arzhk@users.noreply.github.com>
2020-10-08 16:01:48 +0100

    Update script.js
    Detects and rerolls if duplicate number

--- script.js
+++ script.js
@@ -16,7 +16,14 @@ function randomiseNumber() {
   let randomNumber = Math.round(Math.random() * 76);
   let numberArray = document.querySelectorAll(".numberCell");
   let red = document.querySelectorAll(".red");
-
+  if (numberArray[randomNumber - 1].classList.contains("red")) {
+    console.log("duplicate number detected");
+    while (numberArray[randomNumber - 1].classList.contains("red")) {
+      console.log(randomNumber);
+      randomNumber = Math.round(Math.random() * 76);
+      console.log("Rerolled to: " + randomNumber);
+    }
+  }
   const numberHolder = document.querySelector("#randomNumber h2");
   numberHolder.innerText = randomNumber;
   numberArray[parseInt(randomNumber) - 1].classList.add("red");
